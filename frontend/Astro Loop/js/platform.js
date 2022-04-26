edd619005411d362d0700405535d40ef51071ecc
“astrosocietyxyz” <astrosocietyxyz@gmail.com>
2022-04-26 10:41:58 -0400

    revert

--- frontend/Astro Loop/js/platform.js
+++ frontend/Astro Loop/js/platform.js
@@ -141,7 +141,7 @@ class Platform {
         case this.STABLE:
           return color("#7931d6");
         case this.MOVING:
-          return color("getRandomColor()");
+          return color("#31b8d6");
         case this.FRAGILE:
           return color(255);
         default:
